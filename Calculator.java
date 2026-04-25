ccddaff047eac76971af5e50eae21ce7fd996f49
Harsh Raj <harsh933425raj@gmail.com>
2026-04-25 15:45:08 +0530

    Scientific Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,18 @@
 import java.util.*;
+class Calc{
+    void cube(int n){
+        System.out.println("Cube of " + n + (n*n*n));
+    }
+    void Factorial(int n){
+        int fact =1;
+        for(int i=n;i>=1;i--){
+            fact=fact*n;
+
+        }
+        System.out.println("Factorial of " + n + " = " + fact);
+    }
+}
+
  public class Calculator{
 
     public static void main(String[] args) {
@@ -11,6 +25,15 @@ import java.util.*;
         System.out.println("Substraction : " + (a-b));
         System.out.println("Multiplication : " + (a*b));
         System.out.println("Division : " + (a/b));
+        System.out.println("Scientific Calculator");
+        double Square=Math.sqrt(a);
+        System.out.println("Square root of " + a + " : " + Square);
+        double Power = Math.pow(a,b);
+        System.out.println("b to the power a : " + Power);
+        Calc obj = new Calc();
+        obj.Factorial(b);
+        obj.cube(a);
+
         sc.close();
     }
  }
