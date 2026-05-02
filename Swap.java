c9c89db7cfce52b9ebdef4f8bb75db0f95b5f30e
Harsh Raj <harsh933425raj@gmail.com>
2026-05-02 15:38:42 +0530

    Clear message printing

--- Swap.java
+++ Swap.java
@@ -2,10 +2,12 @@ class Swap{
     public static void main(String[] args) {
         int a=4;
         int b=3;
+        System.out.println("Before swap a =" + a);
+        System.out.println("Before swap b = " + b);
         a=a+b;
         b = a-b;
         a=a-b;
-        System.out.println(a);
-        System.out.println(b);       
+        System.out.println("Afrer swap a="+a);
+        System.out.println("After swap b = " + b);       
     }
 }
